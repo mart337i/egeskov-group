732f106bf1102d3b062cf8ba58643ccca9eac9a0
Martin egeskov <*@email.com>
2025-06-29 23:32:55 +0200

    [ADD] Add icon

--- github_integration/__manifest__.py
+++ github_integration/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'GitHub Integration',
-    'version': '18.0.1.0.1',
+    'version': '18.0.1.0.2',
     'category': 'Project',
     'summary': 'Integrate GitHub repositories with Odoo projects',
     'description': """

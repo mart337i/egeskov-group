7e8960cd924291d18c7858de3d10adbbb300be9f
Martin egeskov <mart337i@gmail.com>
2025-07-10 23:43:28 +0200

    [REM] Rem dublicate Controller

--- me_portfolio_website/controllers/main.py
+++ me_portfolio_website/controllers/main.py
@@ -8,7 +8,3 @@ class PortfolioController(http.Controller):
     @http.route('/', type='http', auth='public')
     def portfolio_home(self, **kwargs):
         return request.render('me_portfolio_website.portfolio_landing_page')
-
-    @http.route('/portfolio', type='http', auth='public')
-    def portfolio_page(self, **kwargs):
-        return request.render('me_portfolio_website.portfolio_landing_page')

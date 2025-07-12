300424cc8f54c8029ef1cd1afe9845bb94ca8955
Martin Egeskov <65512800+mart337i@users.noreply.github.com>
2025-07-12 02:18:57 +0200

    Update __manifest__.py

--- me_portfolio_website/__manifest__.py
+++ me_portfolio_website/__manifest__.py
@@ -11,7 +11,7 @@
         
     """,
 
-    'author': "VK DATA ApS",# Categories can be used to filter modules in modules listing
+    'author': "egeskov-group.dk",# Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/16.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'Uncategorized',

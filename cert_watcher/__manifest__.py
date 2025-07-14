b8593caff618c5ce0014c515ed9aa15516c735c0
Martin egeskov <mart337i@gmail.com>
2025-07-14 23:25:39 +0200

    [FIX] Fix cron

--- cert_watcher/__manifest__.py
+++ cert_watcher/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'Certificate Monitor',
-    'version': '18.0.1.0.1',
+    'version': '18.0.1.0.2',
     'category': 'Security',
     'summary': 'Monitor SSL certificates via HTTP requests without local storage',
     'description': """
